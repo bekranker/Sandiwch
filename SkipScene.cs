ccc209bcacd72e16194440a02971812e7bc19933
Bekir <aqwwwe73@gmail.com>
2024-12-08 00:38:29 +0300

    SkipScene

---
+++ SkipScene.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SkipScene : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
